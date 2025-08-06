cf0a790606822d3cc7fa98bd32e7dda61630e9d0
mj <charmblues@gmail.com>
2025-08-06 15:22:35 +0900

    Initial Arduino sketches

--- led/led.ino
+++ led/led.ino
@@ -1,14 +1,21 @@
 int led1=7;
+int led2=6;
+int led3=5;
 
 void setup() {
-  pinMode(7, OUTPUT);
-
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led1, HIGH);
-  delay(500);
+  digitalWrite(led1,HIGH);
+  digitalWrite(led2,HIGH);
+  digitalWrite(led3,HIGH);
+  delay(1000);
 
-  digitalWrite(led1, LOW);
-  delay(500);
+  digitalWrite(led1,LOW);
+  digitalWrite(led2,LOW);
+  digitalWrite(led3,LOW);
+  delay(1000);
 }
