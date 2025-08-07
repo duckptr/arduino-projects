69b9baba36a00e2c78adbc798df2fb454d944032
mj <charmblues@gmail.com>
2025-08-07 10:25:31 +0900

    rgb upgrade

--- rgb/rgb.ino
+++ rgb/rgb.ino
@@ -1,41 +1,54 @@
-int LED1 = 9;
-int LED2 = 10;
-int LED3 = 11;
-int switch1 = 3;
-int switch2 = 2;
-int key_count = 0;
+const int LED1 = 2;
+const int LED2 = 3;
+const int LED3 = 4;
+const int switch1 = 5;
+const int switch2 = 6;
 
+int key_count = 0;
+bool prev1 = false;
+bool prev2 = false;
 
 void setup() 
 {
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
-  pinMode(switch1,INPUT);
-  pinMode(switch2,INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(switch1, INPUT);
+  pinMode(switch2, INPUT);
 
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED3,LOW);
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
 }
 
 void loop() 
 {
- if(digitalRead(switch1) == 1)
- {
-  key_count++;
- }
- if(digitalRead(switch2) == 1)
- {
-  key_count--;
- }
- 
- if(key_count < 0) key_count = 5;
- if(key_count > 5) key_count = 0;
- rainbow(key_count);
- delay(200);
+  // 현재 스위치 상태 읽기
+  bool curr1 = digitalRead(switch1);
+  bool curr2 = digitalRead(switch2);
 
+  // 이전에는 안 눌렸고 지금 눌렸으면 → 상태 전이 감지
+  if (curr1 && !prev1) {
+    key_count++;
+  }
+  if (curr2 && !prev2) {
+    key_count--;
+  }
+
+  // 이전 상태 저장
+  prev1 = curr1;
+  prev2 = curr2;
+
+  // key_count 순환
+  if (key_count < 0) key_count = 5;
+  if (key_count > 5) key_count = 0;
+
+  // 현재 key_count에 따른 동작 실행
+  rainbow(key_count);
+
+  delay(50);  // 빠른 응답을 위해 짧게
 }
+
 
 void rainbow(int kc)
 {
