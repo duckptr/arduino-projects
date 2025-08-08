471b3f44ce8a92b33ad113d07e33d8777482e436
mj <charmblues@gmail.com>
2025-08-08 09:13:37 +0900

    motor

---
+++ servo_moter/servo_moter.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo myservo;
+
+int potentiometer = A0;
+int val;
+
+void setup() 
+{
+  myservo.attach(9);  
+}
+
+void loop() 
+{
+  val = analogRead(potentiometer);
+
+  val = map(val, 0, 1023, 0, 180);
+  myservo.write(val);
+  delay(15);  
+}
--- sound_sensor/sound_sensor.ino
+++ sound_sensor/sound_sensor.ino
@@ -9,11 +9,13 @@ void setup()
 void loop() 
 {
   int value = analogRead(A2); // 소리감지 센서값을 읽어서 변수 value에 저장해줍니다.
+                              // 아날로그 센서값 범위는 0 ~ 1023
+
   // map() 함수 사용, map(value, fromLow, fromHigh, toLow, toHigh)
   // 변환할 수, 현재 범위 값의 하한, 현재 범위 값의 상한, 목표 범위값의 하한, 목표 범위값의 상한
   // 읽어들인 값으로 LED의 밝기를 구하고, LED 밝기를 새로고침 합니다.
   int adjust = map(value,0,1024,0,255);
-  analogWrite(led,adjust);
+  analogWrite(led,adjust); // LED 밝기는 0 ~ 255
   Serial.println(value);
   delay(100);
 }
